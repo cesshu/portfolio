a5c9b889eb3570532edf2640e7bf3043c56b6f74
Cess <pjmende08@gmail.com>
2024-10-07 16:44:32 +0800

    first commit

--- src/pages/Works.js
+++ src/pages/Works.js
@@ -1,14 +1,55 @@
 import React, { useState } from 'react';
-import Modal from '../components/Modal'; // 
+import Modal from '../components/Modal'; 
 import './Works.css';
 
 const projects = [
-  { id: 1, title: 'azure digital designer', date: '2022.05.12', category: 'portfolio', image: 'images/portfolio2.png', description: "Azure Digital Designer is a simple portfolio I created back in 2022, showcasing my journey as a 20-year-old digital designer based in the Philippines. My name is Cess, and I specialize in crafting digital designs for websites, products, and promotions. With experiences spanning from freelance design to in-house writing and web design, I've honed my skills across various platforms and projects. I have a strong foundation in front-end coding, combined with expertise in powerful CMS platforms, making me versatile in both design and development. My portfolio highlights the tools I frequently use, projects I've worked on, and how I can help bring creative ideas to life.", technologies: 'HTML / CSS Grid / Font Awesome / Javascript' },
-  { id: 2, title: 'wandrr travel booking', date: '2023.05.08', category: 'web apps', image: 'images/port5.png', description: "Wandrr Travel Booking is a user-friendly platform designed to simplify the process of planning and booking travel experiences. The idea behind it is to create a one-stop site where users can easily find and reserve their ideal vacations, whether they're looking for a serene beach getaway or a thrilling adventure trek. The platform focuses on providing a seamless experience, backed by a dedicated team of travel experts who offer personalized assistance. Wandrr empowers travelers to turn their dream vacations into reality, offering a wide range of options that cater to diverse preferences.", technologies: 'HTML / CSS / Font Awesome / Bootstrap / Javascript'},
-  { id: 3, title: 'migard hotel reservations', date: '2022.12.18', category: 'web apps', image: 'images/portfolio1.png', description: "Migard Hotel Reservation is a streamlined booking system designed to connect travelers with top-notch accommodations effortlessly. Built around the concept of delivering a worthy experience, Migard ensures guests can find and reserve the perfect hotel for their needs, whether for business or leisure. The platform is committed to offering a seamless booking process, with a focus on quality, comfort, and convenience. With Migard, every stay is more than just a reservation—it's the gateway to a remarkable hotel experience tailored just for you.", technologies: 'HTML / CSS / Bootstrap / Javascript'},
-  { id: 4, title: 'w3schools database', date: '2023.04.17', category: 'back-end', image: 'images/port6.1.png', description: "w3schools database is a project focused on showcasing CRUD (Create, Read, Update, Delete) functionalities within a database environment. The aim of this project is to provide an intuitive interface for managing data, simulating real-world database interactions. It leverages essential database operations to demonstrate how information can be effectively handled, modified, and retrieved. Designed for educational purposes, this project offers a hands-on approach to learning and mastering database management, making it a practical resource for both beginners and experienced developers.", technologies: 'PHP / CSS / Bootstrap / Javascript' },
-  { id: 5, title: 'calendar designs', date: '2023.03.06', category: 'front-end', image: 'images/port8.png', description: "The Calendar is a visually enchanting project inspired by the magical worlds of Studio Ghibli. Created using HTML, CSS, and JavaScript, this calendar is not only functional but also aesthetically designed to bring the charm of Ghibli design. Each month and date is brought to the screen with delicate animations and vibrant colors. Whether you're planning your schedule or simply admiring its design, the Calendar blends practicality with artistic flair, making daily planning an enjoyable experience.", technologies: 'HTML / SCSS / Javascript' },
+  {
+      id: 1,
+      title: 'Azure Digital Designer',
+      date: '2022.05.12',
+      category: 'portfolio',
+      image: `${process.env.PUBLIC_URL}/images/portfolio2.png`,
+      description: "Azure Digital Designer is a simple portfolio I created back in 2022, showcasing my journey as a 20-year-old digital designer based in the Philippines. My name is Cess, and I specialize in crafting digital designs for websites, products, and promotions. With experiences spanning from freelance design to in-house writing and web design, I've honed my skills across various platforms and projects. I have a strong foundation in front-end coding, combined with expertise in powerful CMS platforms, making me versatile in both design and development. My portfolio highlights the tools I frequently use, projects I've worked on, and how I can help bring creative ideas to life.",
+      technologies: 'HTML / CSS Grid / Font Awesome / Javascript',
+  },
+  {
+      id: 2,
+      title: 'Wandrr Travel Booking',
+      date: '2023.05.08',
+      category: 'web apps',
+      image: `${process.env.PUBLIC_URL}/images/port5.png`,
+      description: "Wandrr Travel Booking is a user-friendly platform designed to simplify the process of planning and booking travel experiences. The idea behind it is to create a one-stop site where users can easily find and reserve their ideal vacations, whether they're looking for a serene beach getaway or a thrilling adventure trek. The platform focuses on providing a seamless experience, backed by a dedicated team of travel experts who offer personalized assistance. Wandrr empowers travelers to turn their dream vacations into reality, offering a wide range of options that cater to diverse preferences.",
+      technologies: 'HTML / CSS / Font Awesome / Bootstrap / Javascript',
+  },
+  {
+      id: 3,
+      title: 'Migard Hotel Reservations',
+      date: '2022.12.18',
+      category: 'web apps',
+      image: `${process.env.PUBLIC_URL}/images/portfolio1.png`,
+      description: "Migard Hotel Reservation is a streamlined booking system designed to connect travelers with top-notch accommodations effortlessly. Built around the concept of delivering a worthy experience, Migard ensures guests can find and reserve the perfect hotel for their needs, whether for business or leisure. The platform is committed to offering a seamless booking process, with a focus on quality, comfort, and convenience. With Migard, every stay is more than just a reservation—it's the gateway to a remarkable hotel experience tailored just for you.",
+      technologies: 'HTML / CSS / Bootstrap / Javascript',
+  },
+  {
+      id: 4,
+      title: 'w3schools database',
+      date: '2023.04.17',
+      category: 'back-end',
+      image: `${process.env.PUBLIC_URL}/images/port6.1.png`,
+      description: "w3schools database is a project focused on showcasing CRUD (Create, Read, Update, Delete) functionalities within a database environment. The aim of this project is to provide an intuitive interface for managing data, simulating real-world database interactions. It leverages essential database operations to demonstrate how information can be effectively handled, modified, and retrieved. Designed for educational purposes, this project offers a hands-on approach to learning and mastering database management, making it a practical resource for both beginners and experienced developers.",
+      technologies: 'PHP / CSS / Bootstrap / Javascript',
+  },
+  {
+      id: 5,
+      title: 'Calendar Designs',
+      date: '2023.03.06',
+      category: 'front-end',
+      image: `${process.env.PUBLIC_URL}/images/port8.png`,
+      description: "The Ghibli Calendar is a visually enchanting project inspired by the magical worlds of Studio Ghibli. Created using HTML, CSS, and JavaScript, this calendar is not only functional but also aesthetically designed to bring the charm of Ghibli's iconic animations to life. Each month and date is brought to the screen with delicate animations, vibrant colors, and immersive visuals reminiscent of Ghibli's storytelling. Whether you're planning your schedule or simply admiring its design, the Ghibli Calendar blends practicality with artistic flair, making daily planning an enjoyable experience.",
+      technologies: 'HTML / SCSS / Javascript',
+  },
 ];
+
 
 const Works = () => {
   const [activeFilter, setActiveFilter] = useState('all');
